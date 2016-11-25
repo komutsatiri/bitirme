4ce5cf673e597afe6df78a174ea40ae452c1e3b8
komutsatiri <melihgoksal@gmail.com>
2016-11-25 18:24:15 +0300

    Show only markers by using API

---
+++ api/api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, Response
+from flask_restful import Resource, Api
+from flask.ext.cors import CORS, cross_origin
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+client = MongoClient('localhost',27017)
+
+@app.route('/markers', methods=['GET'])
+def get_markers():
+	collection = client.conflict_db.events
+	output = []
+	for q in collection.find({},{"_id":False}).limit(4000):
+		output.append({'lat' : q['latitude'], 'lon' : q['longitude'], 'dyad_new_id' : q['dyad_new_id']})
+	return jsonify({"results" : output})
+
+
+if __name__ == '__main__':
+    app.run(debug=True )
