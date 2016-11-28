443e05d52f6e9b1301604a2d56be5945bac3127e
komutsatiri <melihgoksal@gmail.com>
2016-11-28 03:23:52 +0300

    Change popup content when mouse get over a pin

--- api/api.py
+++ api/api.py
@@ -9,14 +9,24 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 client = MongoClient('localhost',27017)
+collection = client.conflict_db.events
 
 @app.route('/markers', methods=['GET'])
 def get_markers():
-	collection = client.conflict_db.events
 	output = []
-	for q in collection.find({},{"_id":False}).limit(4000):
+	for q in collection.find({},{'_id':False}).limit(4000):
 		output.append({'id' : q['id'], 'lat' : q['latitude'], 'lon' : q['longitude'], 'dyad_new_id' : q['dyad_new_id']})
-	return jsonify({"results" : output})
+	return jsonify({'results' : output})
+
+@app.route('/details/<int:event_id>', methods=['GET'])
+def get_details(event_id):
+	q = collection.find_one({'id': event_id},{'_id':False})
+	if q:
+		output = {'source_article' : q['source_article']}
+	else:
+		print q
+		output = 'No results found'	
+	return jsonify({'result' : output})
 
 
 if __name__ == '__main__':
