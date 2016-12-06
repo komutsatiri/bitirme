4f801755f6a7a2f5e194ad6c63edddbde525e9a8
komutsatiri <melihgoksal@gmail.com>
2016-12-06 23:34:06 +0300

    Unnecessary lines removed in api.py

--- api/api.py
+++ api/api.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify, Response
-from flask_restful import Resource, Api
+from flask import Flask, jsonify
+#from flask_restful import Resource, Api
 from flask.ext.cors import CORS, cross_origin
 from pymongo import MongoClient
 
 
 app = Flask(__name__)
 CORS(app)
-api = Api(app)
+#api = Api(app)
 client = MongoClient('localhost',27017)
 collection = client.conflict_db.events
+
+@app.route('/merhaba', methods=['GET'])
+def hello_world():
+	output = 'Merhaba !'
+	return jsonify({'result' : output})
 
 @app.route('/markers', methods=['GET'])
 def get_markers():
@@ -41,29 +46,6 @@ def get_dyads():
 	else:
 		output = 'No results found'	
 	return jsonify({'result' : output})	
-
-@app.route('/dyad_name/<int:dyad_new_id>', methods=['GET'])    
-@app.route('/dyad_names', defaults={'dyad_new_id': None}, methods=['GET'])
-def get_dyad_name(dyad_new_id):
-	if dyad_new_id is  None:
-		q = collection.distinct('dyad_name')
-		if q:
-			print q
-			output = q
-		else:
-			print q
-			output = 'No results found'	
-		return jsonify({'result' : output})	
-	elif dyad_new_id is not None:
-		print "debug"
-		q = collection.find_one({'dyad_new_id' : dyad_new_id},{'_id':False, 'dyad_name':True})
-		if q:
-			output = q
-		else:
-			output = 'No results found'
-		return jsonify({'result' : output})			
-
-
 
 if __name__ == '__main__':
     app.run(debug=True )
